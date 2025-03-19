a39c2c67e7d670e643022f34eacb2d85ac78e70a
ahsanzeya <ahsanzeya10@gmail.com>
2025-03-20 00:44:50 +0530

    Addition features Added

--- hello.java
+++ hello.java
@@ -1,8 +1,23 @@
 package HelloWorld;
 
+class Add{
+    /**
+     *
+     */
+    int a=120,b=990;
+    int sum=0;
+    int add(){
+        sum=a+b;
+        System.out.println("Addition: "+sum);
+        return sum;
+    }
+
+}
+
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Add a1=new Add();
+        a1.add();
     }
     
 }
